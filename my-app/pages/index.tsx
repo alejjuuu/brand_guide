a9887c8920b227ffa39e1ee25a3ee0db4ab36c88
Luis V <89930344+alejjuuu@users.noreply.github.com>
2023-11-20 16:04:30 -0500

    new changess

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -2,9 +2,8 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Colors from "../components/Colors";
 import Font from "../components/Font";
-import Testimonials from "@/brand_guide/components/Testimonials";
-import Tabs from "@/brand_guide/components/Tabs";
-import Tabs1 from "@/brand_guide/components/Tabs1";
+import Testimonials from "../components/Testimonials";
+import Tabs from "../components/Tabs";
 import Tabs2 from "../components/Tabs2";
 import "tailwindcss/tailwind.css";
 
