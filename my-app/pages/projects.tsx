e59b3edd871dab2e2372d3a0d5ac9a339b5c4814
Luis V <89930344+alejjuuu@users.noreply.github.com>
2023-11-20 16:19:49 -0500

    importing to vercel

--- my-app/pages/projects.tsx
+++ my-app/pages/projects.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Tabs2";
 import Project from "../components/All_Projects";
-import ProjectSpecs from "@/brand_guide/components/Tabs1";
+//import ProjectSpecs from "@/brand_guide/components/Tabs1";
 
 // Import any other components you need for this page.
 
